7d9fb486142e0ce9629901df4bd5fd3bd6cf55ef
Sarah Harrington <sarahharrington@sarahs-mbp.lan>
2020-06-18 16:57:23 -0500

    added no movie found handling

--- src/App.js
+++ src/App.js
@@ -8,12 +8,17 @@ function App() {
   const [movies, setMovies] = React.useState([]);
   const [inputValue, setInputValue] = React.useState("");
   const [movieToFind, setMovieToFind] = React.useState("");
+  const [movieNotFound, setMovieNotFound] = React.useState("showMovies");
 
   const getMovies = async () => {
     setMovieToFind(inputValue.trim().toLowerCase());
     setInputValue("");
 
   };
+
+  React.useEffect(() => {
+
+  }, [movieNotFound])
 
   React.useEffect(() => {
     if (movieToFind === '') {
@@ -25,7 +30,16 @@ function App() {
         )
         .then(res => res.json())
         .then(res => {
-          setMovies(res.Search);
+          console.log(res.Response)
+          if (res.Response === 'False') {
+            console.log('in the false')
+            setMovieNotFound("hide")
+          }
+          else {
+            console.log('in the true')
+            setMovieNotFound("showMovies")
+            setMovies(res.Search);
+          }
         })
         .catch(e => console.log("error", e));
       }
@@ -44,16 +58,16 @@ function App() {
         />
         <button onClick={getMovies}>Find Movie!</button>
       </div>
-      { 
-        movies.length > 0 && 
-        movies.map(movie => (
-          <MovieListItem 
-            key={movie.imdbID}
-            movie={movie}
-          />
-        ))
+      {movieNotFound === "showMovies" ?
+          movies.length > 0 && 
+          movies.map(movie => (
+            <MovieListItem 
+              key={movie.imdbID}
+              movie={movie}
+          /> 
+        )) :
+        <div>Movie Not Found!</div>
       }
-
     </div>
   );
 }
